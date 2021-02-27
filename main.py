c58a00f1fd030ef7b66f06a2b3a13559f7155f41
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2021-02-27 16:34:55 -0500

    trying to resolve the ip issue

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import numpy as np
 from flask_app.app import flask_app
 
-def main(a, b):
-    print(a)
-    print(b)
+def main():
     fa = flask_app("doggo-dev-chart", settings="settings.json")
     fa.app.run(host="0.0.0.0")
 

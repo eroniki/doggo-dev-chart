4be15655b6e516444bf5e4d0270600c72e9eced1
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2021-02-27 16:29:28 -0500

    change main to accomodate gunicorn

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 from flask_app.app import flask_app
 
-def main():
+def main(a, b):
+    print(a)
+    print(b)
     fa = flask_app("doggo-dev-chart", settings="settings.json")
-    fa.app.run(debug=True)
+    fa.app.run(host="0.0.0.0")
 
 
 if __name__ == "__main__":

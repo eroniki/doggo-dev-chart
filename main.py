95903c4202bb21feaa01888cee832436b0b43b89
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2021-02-27 18:55:16 -0500

    port 5000

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from flask_app.app import flask_app
 
 if __name__ == "__main__":
     fa = flask_app("doggo-dev-chart", settings="settings.json")
-    fa.app.run(host="0.0.0.0", port=80)
+    fa.app.run(host="0.0.0.0", port=5000)
     pass
